883f1b3e24237a8eaa6ab36105e300fdc8eac092
TheSupport101 <jpduarte120@gmail.com>
2019-03-29 01:25:27 +0000

    montecarlo fresquinho, 100% segfault free

--- montecarlo.h
+++ montecarlo.h
@@ -27,29 +27,42 @@ int child_reward_sum(NODE node) {
 
 double ucb(NODE node) {
     double ucb;
-    ucb = child_reward_sum(node) + UCBCONSTANT * (sqrt((2*log(node_get_visits(node->parent)))/node_get_visits(node)));
-    return ucb;
+    if (node->parent != NULL) {
+	ucb = child_reward_sum(node) + UCBCONSTANT * (sqrt((2*log(node_get_visits(node->parent)))/node_get_visits(node)));
+	return ucb;
+    }
+    else {
+	ucb = child_reward_sum(node) + UCBCONSTANT * (sqrt((2/node_get_visits(node))));
+	return ucb;
+    }
 }
 
 NODE select_best(NODE node) {
-    double max = 0;
-    NODE best;
+    double max = -1;
+    NODE best = NULL;
     for (int i=0; i<7; i++) {
 	if (node->children[i] != NULL) {
 	    double temp = ucb(node);
+	    assert(temp>=0);
 	    if (temp > max) {
 		max = temp;
 		best = node->children[i];
 	    }
 	}
     }
+    assert(best);
     return best;
 }
 
 NODE expand_node(NODE node) {
     for (int i=0; i<7; i++) {
 	if (node->children[i] == NULL) {
-	    NODE new = create_node(move(node_get_board(node), random_number(), 1), node);
+	    BOARD movement;
+	    /* VERIFICAR */
+	    do {
+		movement = move(node_get_board(node), random_number(), 1);
+	    } while(!movement);
+	    NODE new = create_node(movement, node);
 	    add_child(node, new);
 	    return new;
 	}
@@ -60,7 +73,7 @@ int rollout(NODE node) {
     /* Should return 1 for a player win and 0 for a computer
        win or a draw. */
     int last_player = 1;
-    BOARD b;
+    BOARD b = node->board;
     int counter = 0;
     while (counter < 10000) {
 	counter++;
@@ -68,11 +81,12 @@ int rollout(NODE node) {
 	   It's now the player's turn. */
 	if (last_player == 1) {
 	    int column = random_number();
-	    if(move(b, column, -1) == NULL){
+	    if (move(b, column, -1) == NULL){
 		continue;
 	    }
 	    /* Making a move and changing player. */
 	    b = move(b, column, -1);
+	    //print_board(b);
 	    last_player = -1;
 	    if(score(b, -1) == -512) {
 		return 1;
@@ -104,6 +118,13 @@ int rollout(NODE node) {
     return 0;
 }
 
+void backpropagate(NODE node, int value, TREE tree) {
+    node_set_reward(node, value);
+    while ((node = node->parent) != NULL) {
+	node_set_visits(node, node_get_visits(node) + 1);
+    }
+}
+
 int montecarlo(BOARD b) {
     /* Intializes random number generator */
     time_t t;
@@ -113,17 +134,60 @@ int montecarlo(BOARD b) {
     NODE root = create_node(b, NULL);
     tree_set_root(tree, root);
 
-    node_set_visits(root, node_get_visits(root) + 1);
-    NODE cur = root;
-    
-    while (node_is_leaf(cur) == 0) {
-	cur = select_best(cur);
-	node_set_visits(cur, node_get_visits(cur) + 1);
-    }
-    
-    NODE new_child = expand_node(cur);
-    node_set_visits(new_child, node_get_visits(new_child) + 1);
+    for (int i=0; i<100; i++) {
+	NODE cur = tree_get_root(tree);
 
-    int value = rollout(new_child);
-    
+	while (node_is_leaf(cur) == 0) {
+	    cur = select_best(cur);
+	    node_set_visits(cur, node_get_visits(cur) + 1);
+	}
+
+	NODE new_child = expand_node(cur);
+	node_set_visits(new_child, node_get_visits(new_child) + 1);
+
+	int value = rollout(new_child);
+	backpropagate(new_child, value, tree);
+    }
+}
+
+int game() {
+    int first = -1;
+    /* Deciding who moves first. */
+    printf("Who will be playing first? (1 for PC, 0 for player)\n");
+    scanf("%d", &first);
+    while(!(first == 0 || first == 1)){
+	printf("Who will be playing first? (1 for PC, 0 for player)\n");
+	scanf("%d", &first);
+    }
+    if(first){
+	b = 
+    }
+
+    while(TRUE){
+	print_board(b);
+	int i = -1;
+	while(i<1 || i>7){
+	    printf("Coluna: ");
+	    scanf("%d", &i);    
+	}
+	if(move(b, i-1, -1) == NULL){
+	    continue;
+	}
+	b = move(b, i-1, -1);
+        
+	if(score(b, -1) == -512){
+	    print_board(b);
+	    printf("**************************\n");
+	    printf("Ganhou!\n");
+	    return 1;
+	}
+	b = 
+	if(score(b, 1) == 512){
+	    print_board(b);
+	    printf("**************************\n");
+	    printf("Perdeu!\n");
+	    return 1;
+	}
+    }
+    return 0;
 }
